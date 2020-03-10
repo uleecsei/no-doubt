4e8279a543a1b9676d283f20a01d6f7db0a7df21
Alla Nesterova <Alla_Nesterova@epam.com>
2020-03-10 18:39:10 +0200

    create scroll block

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -24,3 +24,41 @@ export const initKaraoke = song => {
         currentSong.playAgain();
     });
 };
+
+export const scrollBlocks = () => {
+    if (document.addEventListener) {
+        if ('onwheel' in document) {
+            // IE9+, FF17+, Ch31+
+            document.addEventListener("wheel", onWheel);
+        } else if ('onmousewheel' in document) {
+            // устаревший вариант события
+            document.addEventListener("mousewheel", onWheel);
+        } else {
+            // Firefox < 17
+            document.addEventListener("MozMousePixelScroll", onWheel);
+        }
+    } else { // IE8-
+        document.attachEvent("onmousewheel", onWheel);
+    }
+
+    function onWheel(e) {
+        e = e || window.event;
+        var delta = e.deltaY || e.detail || e.wheelDelta;
+        var prevPosition = 0;
+        var duration = "";
+        if (prevPosition < delta) {
+            duration = "down";
+        } else {
+            duration = "up";
+        }
+
+        if (duration === "down") {
+            document.getElementsByClassName("photo-block__immage")[0].style.marginLeft = "-50%"
+            document.querySelectorAll(".description__top").forEach(e => { e.style.marginTop = "-50%" })
+        } else if (duration === "up") {
+            document.getElementsByClassName("photo-block__immage")[0].style.marginLeft = "0"
+            document.querySelectorAll(".description__top").forEach(e => { e.style.marginTop = "0" })
+        }
+    }
+
+}
