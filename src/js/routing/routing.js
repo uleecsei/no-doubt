89c9564becf0e02cfdfe8c978e2371a6ae4d235a
Alla Nesterova <Alla_Nesterova@epam.com>
2020-03-10 17:03:38 +0200

    save changes on my page

--- src/js/routing/routing.js
+++ src/js/routing/routing.js
@@ -3,5 +3,6 @@ import Route from './route.js';
 
 let router =  new Router([
     new Route('main', 'main.html', true),
-    new Route('ivanpage', 'ivanpage.html')
+    new Route('ivanpage', 'ivanpage.html'),
+    new Route('allapage', 'allapage.html')
 ]);
