39f54075d26168a8dfb77af5f9756144e59ceff7
Oleksii Buchko <Oleksii_Buchko@epam.com>
2020-03-10 16:54:27 +0200

    fixed burger issues

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,9 +1,10 @@
 let buttons = document.getElementsByClassName("burger__button");
 let sideMenu = document.getElementsByClassName("side-menu")[0];
 
-buttons[0].addEventListener("click", function () {
+buttons[0].addEventListener("click", showMenu);
+buttons[1].addEventListener("click", showMenu);
+
+function showMenu() {
     sideMenu.classList.toggle("shown");
-});
-buttons[1].addEventListener("click", function () {
-    sideMenu.classList.toggle("shown");
-});
+}
+
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -4,14 +4,17 @@ window.addEventListener('scroll', function(){
     let header = document.getElementsByClassName('header')[0];
     let logoWrapper = document.getElementsByClassName('header__branding')[0];
     let logo = document.getElementsByClassName('header__title')[0];
+    let sideMenu = document.getElementsByClassName('side-menu')[0];
 
     if(topBorder <= -1 * (newsContainerOffset + header.style.height)){
         logoWrapper.style.height = "0px";
         logo.style.transform = "translateY(-100%)";
+        sideMenu.style.top = "82px";
     }
     else{
         logoWrapper.style.height = "80px";
         logo.style.transform = "translateY(0%)";
+        sideMenu.style.top = "162px";
     }
 });
 
