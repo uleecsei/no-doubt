56b20a844c725abdb98cc956e42269e58fcb5a69
Ivan Dashko <Ivan_Dashko@epam.com>
2020-03-10 17:27:07 +0200

    created functions in router

--- src/js/slider.js
+++ src/js/slider.js
@@ -1,64 +1,60 @@
 "use strict";
 
-// document.addEventListener("DOMContentLoaded", function(){
-//     let slider = new Slider(document.querySelectorAll(".slider")[0]);
-// });
-
-// console.log(document);
-
-class Slider{
-    constructor(container){
+class Slider {
+    constructor(container) {
         this._container = container;
         this._slideIndex = 0;
         this.init();
     }
 
-    init(){
+    init() {
         this.findNodes();
         this.bindAll();
         this.addEvents();
         this.autoRunSlide();
     }
 
-    findNodes(){
+    findNodes() {
         this._nodes = {
             slides: this._container.querySelectorAll(".slider__item"),
             prev: this._container.querySelector(".js-slider-prev"),
             next: this._container.querySelector(".js-slider-next"),
-            dots: this._container.querySelectorAll(".slider-dots__btn")
-        }
+            dots: this._container.querySelectorAll(".slider-dots__btn"),
+        };
     }
 
-    addEvents(){
+    addEvents() {
         this._container.addEventListener("click", this.stop);
         this._nodes.prev.addEventListener("click", this.showPreviousSlide);
         this._nodes.next.addEventListener("click", this.showNextSlide);
-        for (let i = 0; i < this._nodes.dots.length; i++ ) {
-            this._nodes.dots[i].addEventListener("click", ()=>{this.showCurrentSlide(i)});
+        for (let i = 0; i < this._nodes.dots.length; i++) {
+            this._nodes.dots[i].addEventListener("click", () => {
+                this.showCurrentSlide(i);
+            });
         }
     }
 
-    bindAll(){
+    bindAll() {
         this.stop = this.stop.bind(this);
         this.showCurrentSlide = this.showCurrentSlide.bind(this);
         this.showPreviousSlide = this.showPreviousSlide.bind(this);
         this.showNextSlide = this.showNextSlide.bind(this);
     }
 
-    showNextSlide(){
-        this.showSlide(this._slideIndex += 1);
+    showNextSlide() {
+        this.showSlide((this._slideIndex += 1));
     }
 
-    showPreviousSlide(){
-        this.showSlide(this._slideIndex -= 1);
+    showPreviousSlide() {
+        this.showSlide((this._slideIndex -= 1));
     }
 
-    showCurrentSlide(slideNumber){
+    showCurrentSlide(slideNumber) {
         this._slideIndex = slideNumber;
         this.showSlide(slideNumber);
     }
 
-    showSlide(slideNumber){
+    showSlide(slideNumber) {
         if (slideNumber > this._nodes.slides.length - 1) {
             this._slideIndex = 0;
         }
@@ -71,35 +67,40 @@ class Slider{
         this.activateDot();
     }
 
-    activateSlide(){
+    activateSlide() {
         this._nodes.slides[this._slideIndex].style.display = "block";
     }
 
-    activateDot(){
-        this._nodes.dots[this._slideIndex].className += " slider-dots__btn--active";
+    activateDot() {
+        this._nodes.dots[this._slideIndex].className +=
+            " slider-dots__btn--active";
     }
 
-    deActivateDots(){
+    deActivateDots() {
         for (let i = 0; i < this._nodes.dots.length; i++) {
-            this._nodes.dots[i].className = this._nodes.dots[i].className.replace(" slider-dots__btn--active", "");
+            this._nodes.dots[i].className = this._nodes.dots[
+                i
+            ].className.replace(" slider-dots__btn--active", "");
         }
     }
 
-    hideSlides(){
+    hideSlides() {
         for (let i = 0; i < this._nodes.slides.length; i++) {
-                this._nodes.slides[i].style.display = "none";
+            this._nodes.slides[i].style.display = "none";
         }
     }
 
-    start(){
+    start() {
         this.showSlide(this._slideIndex);
     }
 
-    autoRunSlide(){
-        this._timerId = setInterval(()=>{this.showSlide(++this._slideIndex)}, 3000);
+    autoRunSlide() {
+        this._timerId = setInterval(() => {
+            this.showSlide(++this._slideIndex);
+        }, 3000);
     }
 
-    stop(){
+    stop() {
         clearTimeout(this._timerId);
     }
 }
