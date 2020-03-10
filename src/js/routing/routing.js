43f2fcee175c82ef5893cd721c2441e654ae7c6c
Ivan Dashko <Ivan_Dashko@epam.com>
2020-03-10 16:09:18 +0200

    routing have been added,switch beatween main and ivan page

---
+++ src/js/routing/routing.js
@@ -0,0 +1,7 @@
+import Router from './router.js';
+import Route from './route.js';
+
+let router =  new Router([
+    new Route('main', 'main.html', true),
+    new Route('ivanpage', 'ivanpage.html')
+]);
