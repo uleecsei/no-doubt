bbe6811e845cbf9d4fe37b176cb1481aed8e5aa7
Oleksii Buchko <Oleksii_Buchko@epam.com>
2020-03-12 13:19:51 +0200

    burger closing bug fixed

--- src/js/burger.js
+++ src/js/burger.js
@@ -1,5 +1,6 @@
 let buttons = document.getElementsByClassName("burger__button")[0];
 let sideMenu = document.getElementsByClassName("side-menu")[0];
+let navButtons = document.getElementsByClassName("navigation__item");
 
 buttons.addEventListener("click", showMenu);
 
@@ -7,3 +8,10 @@ function showMenu() {
     sideMenu.classList.toggle("shown");
 }
 
+function hideMenu() {
+    sideMenu.classList.remove("shown");
+}
+
+Array.prototype.forEach.call(navButtons, function(item){
+    item.addEventListener("click", hideMenu);
+});
